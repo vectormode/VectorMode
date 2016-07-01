2d8a4e0384ecf65d6b4010269976ced594f751ff
Khan Mohammad Rashedun-Naby <naby88@gmail.com>
2016-07-01 23:45:38 +0600

    Some Models Added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'description'];
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function store_details()
+    {
+        return $this->hasMany('App\Models\Store');
+    }
 }
--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    protected $fillable = ['name', 'phone', 'address', 'description'];
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function store_details()
+    {
+        return $this->hasMany('App\Models\Store');
+    }
 }
